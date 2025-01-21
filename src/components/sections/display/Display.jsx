e5884e8a2b20b388eeff88abb41aaada6c119889
jade <121040221+stephenstrange-blip@users.noreply.github.com>
2025-01-21 20:41:36 +0800

    feat: Add auto-clear function after computing

--- src/components/sections/display/Display.jsx
+++ src/components/sections/display/Display.jsx
@@ -1,17 +1,23 @@
 import './display.css'
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 
-
+const arrOperators = [];
 function Display({ value, operator }) {
   console.log(`Length of values transferred to the display function: ${value.length}`)
+ 
+  arrOperators.push(operator);
+  console.log(`history of operators used ${arrOperators}`)
   if (value.length === 2)
     return (
       <div className='display' key={1}>{value[0] + ' ' + operator + ' ' + value[1]}</div>
     )
-  else
+  else if (operator === "=")
     return (
-      <div className='display' key={1}>{value[0] + ' ' + operator + ' ' + value[1] + ' = ' + value[2]}</div>
+      <div className='display' key={1}>{value[0] + ' ' + arrOperators.at(-2) + ' ' + value[1] + ' = ' + value[2]}</div>
     )
+  else return (
+    <div className='display' key={1}>{value[0] + ' ' + operator + ' ' + value[1] + ' = ' + value[2]}</div>
+  )
 }
 
 
