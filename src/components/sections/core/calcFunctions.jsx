14fc54d5ff681bd51db16e3852e73d1ad773970f
jade <121040221+stephenstrange-blip@users.noreply.github.com>
2025-01-21 20:42:20 +0800

    fix: Display correct operator used after computing

--- src/components/sections/core/calcFunctions.jsx
+++ src/components/sections/core/calcFunctions.jsx
@@ -26,14 +26,14 @@ function operate(num1, operator, num2) {
 
 
 function InputNum() {
-    const [number1, setNum1] = useState('0');
-    const [number2, setNum2] = useState('0');
+    const [number1, setNum1] = useState('');
+    const [number2, setNum2] = useState('');
     const [operator, setOperator] = useState('');
     const [answer, setAnswer] = useState('');
 
     let computedAnswer = '';
-    let num1 = '0';
-    let num2 = '0';
+    let num1 = '';
+    let num2 = '';
     let operation = '';
 
     const inputOperator = (value) => {
@@ -43,8 +43,8 @@ function InputNum() {
             setAnswer(computedAnswer => computedAnswer = operate(num1, operator, num2));
         }
         if (value === "clr"){
-            setNum1(num1 => '0');
-            setNum2(num2 => '0');
+            setNum1(num1 => '');
+            setNum2(num2 => '');
             setAnswer(computedAnswer => '');
             setOperator(operation => '');
         }
@@ -54,6 +54,7 @@ function InputNum() {
         console.log(`num1 and num2 before concat: ${num1} ${num2}`)
         console.log(`operator is ${!operation ? "no value yet" : operation}`)
         console.log(`computed answer is ${!computedAnswer ? "no value yet" : computedAnswer}`)
+
         //https://dmitripavlutin.com/react-hooks-stale-closures/
         //Functional way of updating num1 to avoid state closure
         // setNum(num1 => num1.concat(value))
@@ -62,6 +63,12 @@ function InputNum() {
         }
         else setNum2(num2 => num2.concat(value))
 
+        if (operation === "="){
+            setNum1(num1 => '');
+            setNum2(num2 => '');
+            setAnswer(computedAnswer => '');
+            setOperator(operation => '');
+        }
         
     }
 
